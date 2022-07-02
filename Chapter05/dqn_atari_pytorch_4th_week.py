69897d19c37ad5e8537a4baa8a9a90b91a8c5520
complexhhs <complexhhs@gmail.com>
2022-07-02 14:04:01 +0900

    package added

--- Chapter05/dqn_atari_pytorch_4th_week.py
+++ Chapter05/dqn_atari_pytorch_4th_week.py
@@ -1,41 +1,32 @@
-# %%
-"""
-# Deep Q-Network implementation.
-
-This homework shamelessly demands you to implement a DQN - an approximate q-learning algorithm with experience replay and target networks - and see if it works any better this way.
-
-Original paper:
-https://arxiv.org/pdf/1312.5602.pdf
-"""
-
-# %%
 import sys, os
-# %%
+sys.path.append('..')
+
 import random
 import numpy as np
 import torch
-from material.atari_util import *
+import torch.nn as nn
 
-# %%
+from material.atari_util import *
+from material.replay_buffer import ReplayBuffer
+from material.atari_wrapper import *
+from material.framebuffer import FrameBuffer
+
 import gym
 import numpy as np
 import matplotlib.pyplot as plt
 
+import argparse
+parser = argparse.ArgumentParser(description='Randomseed Argparse')
+parser.add_argument('--seed',type=int,default=123)
+args = parser.parse_args()
 
-# %%
+from gym.core import ObservationWrapper
+from gym.spaces import Box
+
+from PIL import Image # Your code
+
 ENV_NAME = "BreakoutNoFrameskip-v4"
 
-# %%
-"""
-## Preprocessing
-"""
-
-# %%
-"""
-Let's see what observations look like.
-"""
-
-# %%
 env = gym.make(ENV_NAME)
 env.reset()
 
@@ -43,55 +34,21 @@ n_cols = 5
 n_rows = 2
 fig = plt.figure(figsize=(16, 9))
 
-"""
-### Processing game image 
-
-Raw Atari images are large, 210x160x3 by default. However, we don't need that level of detail in order to learn them.
-
-We can thus save a lot of time by preprocessing game image, including
-* Resizing to a smaller shape, 64 x 64
-* Converting to grayscale
-* Cropping irrelevant image parts (top, bottom and edges)
-
-Also please keep one dimension for channel so that final shape would be 1 x 64 x 64.
-
-Tip: You can implement your own grayscale converter and assign a huge weight to the red channel. This dirty trick is not necessary but it will speed up learning.
-"""
-
-# %%
-from gym.core import ObservationWrapper
-from gym.spaces import Box
-
-from PIL import Image # Your code
 
 class PreprocessAtariObs(ObservationWrapper):
     def __init__(self, env):
-        """A gym wrapper that crops, scales image into the desired shapes and grayscales it."""
         ObservationWrapper.__init__(self, env)
 
         self.img_size = (1, 64, 64)
         self.observation_space = Box(0.0, 1.0, self.img_size)
 
     def _to_gray_scale(self, rgb, channel_weights=[0.8, 0.1, 0.1]):
-        #<YOUR CODE>
         dummy = 0
         for idx,channel_weight in enumerate(channel_weights):
             dummy += channel_weight*(rgb[:,:,idx])
         return np.expand_dims(dummy,axis=-1)
     
     def observation(self, img):
-        """what happens to each observation"""
-
-        # Here's what you need to do:
-        #  * crop image, remove irrelevant parts
-        #  * resize image to self.img_size
-        #     (use imresize from any library you want,
-        #      e.g. opencv, skimage, PIL, keras)
-        #  * cast image to grayscale
-        #  * convert image pixels to (0,1) range, float32 type
-        #YOUR CODE>
-      
-        #img = Image.fromarray(np.uint8(img),'L')
         img = Image.fromarray(np.uint8(img),'RGB')
         img = img.resize((64,64))
         img = np.array(img)
@@ -99,8 +56,6 @@ class PreprocessAtariObs(ObservationWrapper):
         return np.array(img,dtype="float32").transpose((2,0,1)) # <YOUR CODE>
 
 # %%
-import gym
-# spawn game instance for tests
 env = gym.make(ENV_NAME)  # create raw env
 env = PreprocessAtariObs(env)
 observation_shape = env.observation_space.shape
@@ -124,54 +79,17 @@ print("Formal tests seem fine. Here's an example of what you'll get.")
 n_cols = 5
 n_rows = 2
 
-# %%
-"""
-### Wrapping.
-"""
-
-# %%
-"""
-**About the game:** You have 5 lives and get points for breaking the wall. Higher bricks cost more than the lower ones. There are 4 actions: start game (should be called at the beginning and after each life is lost), move left, move right and do nothing. There are some common wrappers used for Atari environments.
-"""
-
-# %%
-%load_ext autoreload
-%autoreload 2
-#import atari_wrappers
-from material.atari_wrapper import *
 
 def PrimaryAtariWrap(env, clip_rewards=True):
     assert 'NoFrameskip' in env.spec.id
-
-    # This wrapper holds the same action for <skip> frames and outputs
-    # the maximal pixel value of 2 last frames (to handle blinking
-    # in some envs)
-    #env = atari_wrappers.MaxAndSkipEnv(env, skip=4)
     env = MaxAndSkipEnv(env, skip=4)
-
-    # This wrapper sends done=True when each life is lost
-    # (not all the 5 lives that are givern by the game rules).
-    # It should make easier for the agent to understand that losing is bad.
-    #env = atari_wrappers.EpisodicLifeEnv(env)
     env = EpisodicLifeEnv(env)
-
-    # This wrapper laucnhes the ball when an episode starts.
-    # Without it the agent has to learn this action, too.
-    # Actually it can but learning would take longer.
-    #env = atari_wrappers.FireResetEnv(env)
     env = FireResetEnv(env)
-
-    # This wrapper transforms rewards to {-1, 0, 1} according to their sign
     if clip_rewards:
-        #env = atari_wrappers.ClipRewardEnv(env)
         env = ClipRewardEnv(env)
-
-    # This wrapper is yours :)
     env = PreprocessAtariObs(env)
     return env
 
-# %%
-from material.framebuffer import FrameBuffer
 
 def make_env(clip_rewards=True, seed=None):
     env = gym.make(ENV_NAME)  # create raw env
@@ -186,36 +104,18 @@ env.reset()
 n_actions = env.action_space.n
 state_shape = env.observation_space.shape
 
-"""
-## DQN as it is
-"""
-
-# %%
-import torch
-import torch.nn as nn
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# %%
 def conv2d_size_out(size, kernel_size, stride):
-    """
-    common use case:
-    cur_layer_img_w = conv2d_size_out(cur_layer_img_w, kernel_size, stride)
-    cur_layer_img_h = conv2d_size_out(cur_layer_img_h, kernel_size, stride)
-    to understand the shape for dense layer's input
-    """
     return (size - (kernel_size - 1) - 1) // stride  + 1
 
-
-# %%
 class DQNAgent(nn.Module):
     def __init__(self, state_shape, n_actions, epsilon=0):
-
         super().__init__()
         self.epsilon = epsilon
         self.n_actions = n_actions
         self.state_shape = state_shape
 
-        # Define your network body here. Please make sure agent is fully contained here
         self.seq = nn.Sequential(
             nn.Conv2d(4,16,3,stride=2), # [batch,16,32,32]
             nn.ReLU(),
@@ -231,51 +131,32 @@ class DQNAgent(nn.Module):
         
 
     def forward(self, state_t):
-        """
-        takes agent's observation (tensor), returns qvalues (tensor)
-        :param state_t: a batch of 4-frame buffers, shape = [batch_size, 4, h, w]
-        """
-        # Use your network to compute qvalues for given state
         qvalues = self.seq(state_t)
-        
         assert qvalues.requires_grad, "qvalues must be a torch tensor with grad"
-        assert len(
-            qvalues.shape) == 2 and qvalues.shape[0] == state_t.shape[0] and qvalues.shape[1] == n_actions
-
+        assert len(qvalues.shape) == 2 and qvalues.shape[0] == state_t.shape[0] and qvalues.shape[1] == n_actions
         return qvalues
 
     def get_qvalues(self, states):
-        """
-        like forward, but works on numpy arrays, not tensors
-        """
         model_device = next(self.parameters()).device
         states = torch.tensor(states, device=model_device, dtype=torch.float)
         qvalues = self.forward(states)
         return qvalues.data.cpu().numpy()
 
     def sample_actions(self, qvalues):
-        """pick actions given qvalues. Uses epsilon-greedy exploration strategy. """
         epsilon = self.epsilon
         batch_size, n_actions = qvalues.shape
 
         random_actions = np.random.choice(n_actions, size=batch_size)
         best_actions = qvalues.argmax(axis=-1)
 
-        should_explore = np.random.choice(
-            [0, 1], batch_size, p=[1-epsilon, epsilon])
+        should_explore = np.random.choice([0, 1],
+                                            batch_size,
+                                            p=[1-epsilon, epsilon])
         return np.where(should_explore, random_actions, best_actions)
 
-# %%
 agent = DQNAgent(state_shape, n_actions, epsilon=0.5).to(device)
 
-# %%
-"""
-Now let's try out our agent to see if it raises any errors.
-"""
-
-# %%
 def evaluate(env, agent, n_games=1, greedy=False, t_max=10000):
-    """ Plays n_games full games. If greedy, picks actions as argmax(qvalues). Returns mean reward. """
     rewards = []
     for _ in range(n_games):
         s = env.reset()
@@ -291,14 +172,7 @@ def evaluate(env, agent, n_games=1, greedy=False, t_max=10000):
         rewards.append(reward)
     return np.mean(rewards)
 
-# %%
-print(f'Evaluate: {evaluate(env, agent, n_games=1)}')
 
-
-# %%
-from material.replay_buffer import ReplayBuffer
-
-# %%
 def play_and_record(initial_state, agent, env, exp_replay, n_steps=1):
     """
     Play the game for exactly n steps, record every (s,a,r,s', done) to replay buffer. 
@@ -326,52 +200,9 @@ def play_and_record(initial_state, agent, env, exp_replay, n_steps=1):
             
     return sum_rewards, s
 
-# %%
-"""
-### Target networks
-
-We also employ the so called "target network" - a copy of neural network weights to be used for reference Q-values:
-
-The network itself is an exact copy of agent network, but it's parameters are not trained. Instead, they are moved here from agent's actual network every so often.
-
-$$ Q_{reference}(s,a) = r + \gamma \cdot \max _{a'} Q_{target}(s',a') $$
-
-![img](https://github.com/yandexdataschool/Practical_RL/raw/master/yet_another_week/_resource/target_net.png)
-"""
-
-# %%
 target_network = DQNAgent(agent.state_shape, agent.n_actions, epsilon=0.5).to(device)
-# This is how you can load weights from agent into target network
 target_network.load_state_dict(agent.state_dict())
 
-# %%
-"""
-### Learning with... Q-learning
-Here we write a function similar to `agent.update` from tabular q-learning.
-"""
-
-# %%
-"""
-Compute Q-learning TD error:
-
-$$ L = { 1 \over N} \sum_i [ Q_{\theta}(s,a) - Q_{reference}(s,a) ] ^2 $$
-
-With Q-reference defined as
-
-$$ Q_{reference}(s,a) = r(s,a) + \gamma \cdot max_{a'} Q_{target}(s', a') $$
-
-Where
-* $Q_{target}(s',a')$ denotes q-value of next state and next action predicted by __target_network__
-* $s, a, r, s'$ are current state, action, reward and next state respectively
-* $\gamma$ is a discount factor defined two cells above.
-
-
-__Note 1:__ there's an example input below. Feel free to experiment with it before you write the function.
-
-__Note 2:__ compute_td_loss is a source of 99% of bugs in this homework. If reward doesn't improve, it often helps to go through it line by line [with a rubber duck](https://rubberduckdebugging.com/).
-"""
-
-# %%
 def compute_td_loss(states, actions, rewards, next_states, is_done,
                     agent, target_network,
                     gamma=0.99,
@@ -380,10 +211,8 @@ def compute_td_loss(states, actions, rewards, next_states, is_done,
     """ Compute td loss using torch operations only. Use the formulae above. """
     states = torch.tensor(states, device=device, dtype=torch.float)    # shape: [batch_size, *state_shape]
 
-    # for some torch reason should not make actions a tensor
     actions = torch.tensor(actions, device=device, dtype=torch.long)    # shape: [batch_size]
     rewards = torch.tensor(rewards, device=device, dtype=torch.float)  # shape: [batch_size]
-    # shape: [batch_size, *state_shape]
     next_states = torch.tensor(next_states, device=device, dtype=torch.float)
     is_done = torch.tensor(
         is_done.astype('float32'),
@@ -392,29 +221,19 @@ def compute_td_loss(states, actions, rewards, next_states, is_done,
     )  # shape: [batch_size]
     is_not_done = 1 - is_done
 
-    # get q-values for all actions in current states
     predicted_qvalues = agent(states)
-
-    # compute q-values for all actions in next states
     predicted_next_qvalues = target_network(next_states)
     
-    # select q-values for chosen actions
     predicted_qvalues_for_actions = predicted_qvalues[range(
         len(actions)), actions]
 
-    # compute V*(next_states) using predicted next q-values
-    #<YOUR CODE>
     next_state_values = predicted_next_qvalues.max(dim=-1).values
 
     assert next_state_values.dim(
     ) == 1 and next_state_values.shape[0] == states.shape[0], "must predict one value per state"
 
-    # compute "target q-values" for loss - it's what's inside square parentheses in the above formula.
-    # at the last state use the simplified formula: Q(s,a) = r(s,a) since s' doesn't exist
-    # you can multiply next state values by is_not_done to achieve this.
     target_qvalues_for_actions = rewards+is_not_done*gamma*next_state_values
 
-    # mean squared error loss to minimize
     loss = torch.mean((predicted_qvalues_for_actions -
                        target_qvalues_for_actions.detach()) ** 2)
 
@@ -428,12 +247,7 @@ def compute_td_loss(states, actions, rewards, next_states, is_done,
 
     return loss
 
-# %%
-"""
-Sanity checks
-"""
 
-# %%
 obs_batch, act_batch, reward_batch, next_obs_batch, is_done_batch = exp_replay.sample(
     10)
 
@@ -456,7 +270,8 @@ from tqdm import trange
 from IPython.display import clear_output
 
 # %%
-seed = 123 #<YOUR CODE: your favourite random seed>
+#seed = 123 #<YOUR CODE: your favourite random seed>
+seed = args.seed
 random.seed(seed)
 np.random.seed(seed)
 torch.manual_seed(seed)
@@ -493,12 +308,11 @@ for i in range(100):
     play_and_record(state, agent, env, exp_replay, n_steps=10**2)
     if len(exp_replay) == 10**4:
         break
-print(len(exp_replay))
 
 # %%
 timesteps_per_epoch = 1
 batch_size = 16
-total_steps = 3 * 10**6
+total_steps = 10 * 10**6
 decay_steps = 10**6
 
 opt = torch.optim.Adam(agent.parameters(), lr=1e-4)
@@ -522,12 +336,13 @@ initial_state_v_history = []
 step = 0
 
 # %%
+RW_record = 0
 state = env.reset()
 for step in range(step, total_steps + 1):
     #if not utils.is_enough_ram():
     if not is_enough_ram():
         print('less that 100 Mb RAM available, freezing')
-        print('make sure everythin is ok and make KeyboardInterrupt to continue')
+        print('make sure everything is ok and make KeyboardInterrupt to continue')
         try:
             while True:
                 pass
@@ -569,15 +384,13 @@ for step in range(step, total_steps + 1):
         print(f'------------------------------')
         print(f'Epoch: {step}, Information')
         print(f'Mean RW_history: {Mean_rw_hist}')
-        print(f'Initial Q value: {np.max(initial_state_q_values}')
+        print(f'Initial Q value: {np.max(initial_state_q_values)}')
         print('')
-        torch.save(agent,f'./ckpt/ckpt_{step}_Atari_model.pth')
 
+    if Mean_rw_hist > RW_record and (step+1) % 1e+03 == 0:
+        print('Check point save')
+        torch.save(agent,f'./ckpt/Randomseed_{seed}_ckpt_{step}_Atari_model.pth')
 
-# %%
-"""
-Final scoring is done on a whole episode with all 5 lives.
-"""
 
 # %%
 final_score = evaluate(
