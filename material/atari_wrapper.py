b2b3d9c82da963463efe44cb2bf720f17ef3408b
complexhhs <complexhhs@gmail.com>
2022-10-09 22:51:48 +0900

    2022-10-09_updated

--- material/atari_wrapper.py
+++ material/atari_wrapper.py
@@ -103,6 +103,49 @@ class EpisodicLifeEnv(gym.Wrapper):
         return obs
 
 
+class EpisodicLifeEnv2(gym.Wrapper):
+    def __init__(self, env):
+        """Make 0 life == end-of-episode, but only reset on true game over.
+        Done by DeepMind for the DQN and co. since it helps value estimation.
+        """
+        gym.Wrapper.__init__(self, env)
+        self.lives = 5
+        self.was_real_done = True
+
+    def step(self, action):
+        obs, reward, done, info = self.env.step(action)
+        #self.was_real_done = done
+        # check current lives, make loss of life terminal,
+        # then update lives to handle bonus lives
+        lives = self.env.unwrapped.ale.lives()
+        #if lives < self.lives and lives > 0:
+            # for Qbert sometimes we stay in lives == 0 condition for a few frames
+            # so it's important to keep lives > 0, so that we only reset once
+            # the environment advertises done.
+            #done = True
+        # auto matically shot when One-life lost
+        if self.lives < self.env.unwrapped.ale.lives():
+            _,_,_,_ = env.step(1)
+            self.lives = lives
+        if lives == 0:
+            self.was_real_done=True
+        return obs, reward, done, info
+
+    def reset(self, **kwargs):
+        """Reset only when lives are exhausted.
+        This way all states are still reachable even though lives are episodic,
+        and the learner need not know about any of this behind-the-scenes.
+        """
+        if self.was_real_done:
+            obs = self.env.reset(**kwargs)
+        else:
+            # no-op step to advance from terminal/lost life state
+            obs, _, _, _ = self.env.step(0)
+        self.lives = self.env.unwrapped.ale.lives()
+        return obs
+
+
+
 # in torch imgs have shape [c, h, w] instead of common [h, w, c]
 class AntiTorchWrapper(gym.ObservationWrapper):
     def __init__(self, env):
